7e7753bd2e23c096f779d1c0df70bc0a78d76fe8
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2011-05-08 08:15:08 +0530

    Starting out, fancy emacs quote

---
+++ init.el
@@ -0,0 +1,7 @@
+;; Insert fancy emacs quotes here
+
+;; "Emacs outshines all other editing software in approximately the
+;; same way that the noonday sun does the stars. It is not just bigger
+;; and brighter; it simply makes everything else vanish."
+;; -Neal Stephenson, "In the Beginning was the Command Line"
+;; -https://github.com/technomancy/emacs-starter-kit/blob/master/init.el
