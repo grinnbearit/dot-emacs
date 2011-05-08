d759a9e142a5e1f402662241a8c22b6363cb1d36
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2011-05-08 13:25:30 +0530

    Added package.el

--- init.el
+++ init.el
@@ -6,8 +6,17 @@
 ;; -Neal Stephenson, "In the Beginning was the Command Line"
 ;; -https://github.com/technomancy/emacs-starter-kit/blob/master/init.el
 
+
+;; external repos and downloads
+(add-to-list 'load-path "~/.emacs.d/plugins")
+;; http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+
 (add-to-list 'load-path "~/.emacs.d/config")  ; custom configurations
-(add-to-list 'load-path "~/.emacs.d/plugins") ; external repos and downloads
 (load "theme")				      ; appearance
 (load "defaults")			      ; defaults
 (load "magit")				      ; magit
