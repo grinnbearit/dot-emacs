e63a55ead4f7fbc6f979f7c6e9676585c4fee656
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2011-05-09 16:41:13 +0530

    FIX: clojure mode symbol lookup

--- config/clojure.el
+++ config/clojure.el
@@ -2,7 +2,5 @@
 
 
 ;; Enable paredit in clojure
-(defun clojure-paredit-hook () (require 'paredit) (paredit-mode +1))
+(defun clojure-paredit-hook () (require 'paredit) (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'clojure-paredit-hook)
-(define-key clojure-mode-map "{" 'paredit-open-brace)
-(define-key clojure-mode-map "}" 'paredit-close-brace)
