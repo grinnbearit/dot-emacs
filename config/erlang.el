12135a95f3f986de6d35c8c1eeaf4d4aad04d749
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2011-06-29 19:28:06 +0530

    added distel

--- config/erlang.el
+++ config/erlang.el
@@ -1,3 +1,7 @@
 ;; Require erlang
 ;; https://github.com/massemanet/distel
 (add-to-list 'load-path (expand-path "plugins/erlang"))
+
+(add-to-list 'load-path (expand-path "plugins/distel/elisp"))
+(require 'distel)
+(distel-setup)
