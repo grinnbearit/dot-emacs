fd6963e2ead3292174e2b301c89f900900516742
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2011-09-22 18:19:08 +0530

    mo-git-blame

--- config/magit.el
+++ config/magit.el
@@ -8,3 +8,9 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 
+;; mo-git-blame
+;; http://www.bunkus.org/blog/2009/10/an-interactive-iterative-git-blame-mode-for-emacs/
+(add-to-list 'load-path (expand-path "plugins/mo-git-blame"))
+(autoload 'mo-git-blame-file "mo-git-blame" nil t)
+(autoload 'mo-git-blame-current "mo-git-blame" nil t)
+
