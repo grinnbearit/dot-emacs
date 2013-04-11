61aa85b143cfe96901e120fc7dc24456cdbe4e63
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2013-04-11 11:36:08 +0530

    persistent nrepl history

--- config/clojure.el
+++ config/clojure.el
@@ -6,3 +6,8 @@
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
 (add-hook 'nrepl-mode-hook 'nrepl-enable-on-existing-clojure-buffers)
+
+
+;; persistent nrepl history
+;; https://groups.google.com/forum/#!msg/nrepl-el/Nbaeaqw_VBQ/iw9vkvJKyI0J
+(setq nrepl-history-file "~/.nrepl-history.eld")
