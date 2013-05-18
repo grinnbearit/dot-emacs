8a7ee8cda7796845b4b491b8749ef6c23456ef11
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2013-05-18 10:42:02 +0530

    new nrepl config

--- config/clojure.el
+++ config/clojure.el
@@ -2,12 +2,32 @@
 
 (require 'paredit)
 
-
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
 (add-hook 'nrepl-mode-hook 'nrepl-enable-on-existing-clojure-buffers)
 
-
+;; configure nrepl
+;; http://ianeslick.com/2013/05/17/clojure-debugging-13-emacs-nrepl-and-ritz/
 ;; persistent nrepl history
 ;; https://groups.google.com/forum/#!msg/nrepl-el/Nbaeaqw_VBQ/iw9vkvJKyI0J
+
+(require 'nrepl)
+(setq nrepl-hide-special-buffers t)
+(setq nrepl-popup-stacktraces-in-repl t)
 (setq nrepl-history-file "~/.nrepl-history.eld")
+
+; Some default eldoc facilities
+(add-hook 'nrepl-connected-hook
+          (defun pnh-clojure-mode-eldoc-hook ()
+            (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
+            (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+            (nrepl-enable-on-existing-clojure-buffers)))
+
+;; Repl mode hook
+(add-hook 'nrepl-mode-hook 'subword-mode)
+
+;; Auto completion for NREPL
+(require 'ac-nrepl)
+(eval-after-load "auto-complete"
+'(add-to-list 'ac-modes 'nrepl-mode))
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
