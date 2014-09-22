f1fb54fa414eb1fc69527219d2f8e13e849e19c0
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2014-09-22 09:40:48 +0530

    melpa package repository

--- config/package.el
+++ config/package.el
@@ -9,6 +9,8 @@
 ;; External repos
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 
 ;; Initialize installed packages
