ae623ca0f0d19035ab511954c39f10777e9f5f90
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2019-05-07 21:04:10 +0200

    Clojure Mode and Cider

--- init.el
+++ init.el
@@ -6,14 +6,6 @@
 ;; -Neal Stephenson, "In the Beginning was the Command Line"
 ;; -https://github.com/technomancy/emacs-starter-kit/blob/master/init.el
 
-
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (defun expand-path (path)
   (concat user-emacs-directory path))
 
