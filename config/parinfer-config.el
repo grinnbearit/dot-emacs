28cab9be58745a825c57167b8a3fbbffdcb8ba94
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2019-05-13 21:45:26 +0200

    Paredit mode added to all parinfer hooks

--- config/parinfer-config.el
+++ config/parinfer-config.el
@@ -13,7 +13,14 @@
             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
             smart-yank))   ; Yank behavior depend on mode.
     (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'clojure-mode-hook #'paredit-mode)
+    (add-hook 'cider-repl-mode-hook #'parinfer-mode)
+    (add-hook 'cider-repl-mode-hook #'paredit-mode)
     (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'paredit-mode)
     (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'paredit-mode)
     (add-hook 'scheme-mode-hook #'parinfer-mode)
-    (add-hook 'lisp-mode-hook #'parinfer-mode)))
+    (add-hook 'scheme-mode-hook #'paredit-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'paredit-mode)))
